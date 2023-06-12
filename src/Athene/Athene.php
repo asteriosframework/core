bb4723eca718c77ef6f17935ec0774825f05a46a
Joerg Heinrich <joerg@jfheinrich.eu>
2023-06-12 16:58:03 +0200

    AF-14: Console program added

--- src/Athene/Athene.php
+++ src/Athene/Athene.php
@@ -11,30 +11,37 @@ use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\DBAL\DriverManager;
 
+// use Doctrine\ORM\Tools\Console\ConsoleRunner;
+// use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+
+
 class Athene
 {
-    protected string $config_file = __DIR__ . '/../config/athene.php';
     protected array $config = [];
     protected EntityManager $entityManager;
 
     public function __construct(
-        protected Configuration $configuration,
-        protected Connection $connection
+        protected Configuration|null $configuration = null,
+        protected Connection|null $connection = null
     ) {
-        $this->config = Config::load($this->config_file);
+        $this->config = Config::load('athene');
 
-        $this->configuration = ORMSetup::createAttributeMetadataConfiguration(
-            paths: [__DIR__ . '/../Models'],
-            isDevMode: true,
-        );
+        if (null === $configuration) {
+            $this->configuration = ORMSetup::createAttributeMetadataConfiguration(
+                paths: [__DIR__ . '/../Models'],
+                isDevMode: true,
+            );
+        }
 
-        $this->connection = DriverManager::getConnection([
-            'dbname' => $this->config['dbname'],
-            'user' => $this->config['user'],
-            'password' => $this->config['password'],
-            'host' => $this->config['host'],
-            'driver' => $this->config['driver'],
-        ]);
+        if (null === $configuration) {
+            $this->connection = DriverManager::getConnection([
+                'dbname' => $this->config['dbname'],
+                'user' => $this->config['user'],
+                'password' => $this->config['password'],
+                'host' => $this->config['host'],
+                'driver' => $this->config['driver'],
+            ]);
+        }
 
         $this->entityManager = new EntityManager($this->connection, $this->configuration);
     }
---
+++ src/config/athene.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dbname' => 'db',
+    'user' => 'db',
+    'password' => 'db',
+    'host' => 'db',
+    'driver' => 'pdo_mysql'
+];
