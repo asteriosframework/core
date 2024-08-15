941ed3108e14c87169968fc7ca5ef882dfbab972
asteriosphpteam <info@asteriosphp.de>
2024-08-15 21:09:22 +0200

    feat: add Collection to asterios core

---
+++ src/Collection.php
@@ -0,0 +1,127 @@
+<?php declare(strict_types=1);
+
+namespace Asterios\Core;
+
+use ArrayAccess;
+use ArrayIterator;
+use Asterios\Core\Exception\CollectionException;
+use Closure;
+use Countable;
+use IteratorAggregate;
+
+class Collection implements ArrayAccess, Countable, IteratorAggregate
+{
+    protected array $items = [];
+
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    public static function forge(array $items = []): self
+    {
+        return new self($items);
+    }
+
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    public function add($item): self
+    {
+        $this->items[] = $item;
+
+        return $this;
+    }
+
+    public function map(Closure $callback): self
+    {
+        $this->items = array_map($callback, $this->items);
+
+        return $this;
+    }
+
+    public function filter(Closure $callback): self
+    {
+        $this->items = array_filter($this->items, $callback);
+
+        return $this;
+    }
+
+    public function reduce(Closure $callback, $initial = null)
+    {
+        return array_reduce($this->items, $callback, $initial);
+    }
+
+    public function first(Closure $callback = null, $default = null)
+    {
+        foreach ($this->items as $item)
+        {
+            if ($callback === null || $callback($item))
+            {
+                return $item;
+            }
+        }
+
+        return $default;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset): mixed
+    {
+        return $this->items[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        if ($offset === null)
+        {
+            $this->items[] = $value;
+        }
+        else
+        {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->items[$offset]);
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    /**
+     * @return string
+     * @throws CollectionException
+     */
+    public function toJson(): string
+    {
+        try
+        {
+            return json_encode($this->items, JSON_THROW_ON_ERROR);
+        }
+        catch (\JsonException $e)
+        {
+            throw new CollectionException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function toArray(): array
+    {
+        return $this->items;
+    }
+}
---
+++ tests/CollectionTest.php
@@ -0,0 +1,214 @@
+<?php declare(strict_types=1);
+
+namespace Asterios\Test;
+
+use Asterios\Core\Collection;
+use Asterios\Core\Exception\CollectionException;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class CollectionTest extends MockeryTestCase
+{
+    protected Collection $testedClass;
+
+    public function testAll(): void
+    {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->all();
+
+        self::assertEquals($data, $actual);
+    }
+
+    public function testAdd(): void
+    {
+        $data = [];
+
+        $expectedData = array_merge($data, ['myTest']);
+
+        $this->testedClass = Collection::forge($data);
+        $this->testedClass->add('myTest');
+
+        $actual = $this->testedClass->all();
+
+        self::assertEquals($expectedData, $actual);
+    }
+
+    public function testMap(): void
+    {
+        $data = [1];
+
+        $this->testedClass = Collection::forge($data);
+
+        $this->testedClass->map(function ($item) {
+            return $item * 2;
+        });
+
+        $actual = $this->testedClass->offsetGet(0);
+
+        self::assertEquals(2, $actual);
+    }
+
+    public function testFilter(): void
+    {
+        $data = [1 => 'John', 2 => 'Jim', 3 => 'Joe', 4 => 'Jolie'];
+
+        $this->testedClass = Collection::forge($data);
+
+        $this->testedClass->filter(function ($item) {
+            return $item === 'Jolie';
+        });
+
+        $filtered = $this->testedClass->all();
+
+        self::assertEquals([4 => 'Jolie'], $filtered);
+    }
+
+    public function testReduce(): void
+    {
+
+        $this->testedClass = Collection::forge([1, 2, 3, 4, 5]);
+
+        $actual = $this->testedClass->reduce(function ($carry, $item) {
+            return $carry + $item;
+        }, 0);
+
+        self::assertEquals(15, $actual);
+    }
+
+    public function testFirst(): void
+    {
+        $data = [12345, 67890];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->first();
+
+        self::assertEquals(12345, $actual);
+    }
+
+    public function testFirstWithDefault(): void
+    {
+        $data = ['Jim'];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->first(function ($item) {
+            return $item === 'John';
+        }, 98765);
+
+        self::assertEquals(98765, $actual);
+    }
+
+    public function testOffsetExists(): void
+    {
+        $data = [12345 => ['username' => 'john'], 67890 => ['username' => 'jim']];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->offsetExists(67890);
+
+        self::assertTrue($actual);
+    }
+
+    public function testoffsetSetWithoutOffset(): void
+    {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+
+        $expectedData = array_merge($data, [4]);
+
+        $this->testedClass = Collection::forge($data);
+        $this->testedClass->offsetSet(null, 4);
+
+        $actual = $this->testedClass->all();
+
+        self::assertEquals($expectedData, $actual);
+    }
+
+    public function testoffsetSetWithOffset(): void
+    {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+
+        $expectedData = array_merge($data, ['d' => 4]);
+
+        $this->testedClass = Collection::forge($data);
+        $this->testedClass->offsetSet('d', 4);
+
+        $actual = $this->testedClass->all();
+
+        self::assertEquals($expectedData, $actual);
+    }
+
+    public function testoffsetUnset(): void
+    {
+        $data = [12345 => ['username' => 'john'], 67890 => ['username' => 'jim']];
+
+        $this->testedClass = Collection::forge($data);
+
+        $this->testedClass->offsetUnset(67890);
+
+        $expectedData = [12345 => ['username' => 'john']];
+
+        $actual = $this->testedClass->all();
+
+        self::assertEquals($expectedData, $actual);
+    }
+
+    public function testCount(): void
+    {
+        $data = ['a', 'b', 'c'];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->count();
+
+        self::assertEquals(3, $actual);
+    }
+
+    public function testGetIterator(): void
+    {
+        $data = [12345 => ['username' => 'john'], 67890 => ['username' => 'jim']];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->getIterator();
+
+        self::assertInstanceOf(\ArrayIterator::class, $actual);
+    }
+    
+    public function testToJson(): void
+    {
+        $data = [12345 => ['username' => 'john'], 67890 => ['username' => 'jim']];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->toJson();
+
+        self::assertEquals(json_encode($data, JSON_THROW_ON_ERROR), $actual);
+    }
+
+    public function testToJsonWithException(): void
+    {
+        $this->expectException(CollectionException::class);
+
+        $resource = fopen('php://temp', 'rb');
+        $this->testedClass = Collection::forge(['resource' => $resource]);
+
+        $this->testedClass->toJson();
+
+        fclose($resource);
+    }
+
+    public function testToArray(): void
+    {
+        $data = [12345, 67890];
+
+        $this->testedClass = Collection::forge($data);
+
+        $actual = $this->testedClass->toArray();
+
+        self::assertEquals([12345, 67890], $actual);
+    }
+
+}
