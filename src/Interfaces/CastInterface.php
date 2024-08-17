cf074a0c547c6394ecb8dffcb9f386bd93c25f30
asteriosphpteam <info@asteriosphp.de>
2024-08-17 16:17:35 +0200

    feat: add Cast class

---
+++ src/Interfaces/CastInterface.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Asterios\Core\Interfaces;
+
+interface CastInterface
+{
+    /**
+     * @param mixed $value
+     * @return int
+     */
+    public function int(mixed $value): int;
+
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public function string(mixed $value): string;
+
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    public function bool(mixed $value): bool;
+
+    /**
+     * @param mixed $value
+     * @return float
+     */
+    public function double(mixed $value): float;
+
+    /**
+     * @param mixed $value
+     * @return float
+     */
+    public function float(mixed $value): float;
+
+    /**
+     * @param array $value
+     * @return object
+     */
+    public function object(array $value): object;
+
+}
