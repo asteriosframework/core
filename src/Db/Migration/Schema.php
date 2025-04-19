10c5b007aee019ce6fa523f5a5d19c8b7aa980e8
asteriosphpteam <info@asteriosphp.de>
2025-04-19 18:09:39 +0200

    feat: logger for debugging [skip-ci]

--- src/Db/Migration/Schema.php
+++ src/Db/Migration/Schema.php
@@ -6,6 +6,7 @@ use Asterios\Core\Db;
 use Asterios\Core\Db\Builder\SchemaBuilder;
 use Asterios\Core\Exception\ConfigLoadException;
 use Asterios\Core\Exception\MigrationException;
+use Asterios\Core\Logger;
 use Closure;
 
 class Schema
@@ -27,6 +28,8 @@ class Schema
 
         $sql = "CREATE TABLE `$table` (\n" . $sqlStatements . "\n) ENGINE=$engine DEFAULT CHARSET=$charSet;";
 
+        Logger::forge()
+            ->info($sql);
         try
         {
             Db::write($sql);
